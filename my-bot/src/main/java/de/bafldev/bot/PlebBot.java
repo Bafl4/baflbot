54bb5dccea3634996cd6ea38487ccaf77271ec92
Ryan <thelasmaster444@gmail.com>
2017-06-18 20:20:42 +0200

    dependecy things and PlebBot

---
+++ my-bot/src/main/java/de/bafldev/bot/PlebBot.java
@@ -0,0 +1,10 @@
+package de.bafldev.bot;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+public class PlebBot{
+    public static void main(String[] args){
+
+        JDABuilder  builder = new JDABuilder(AccountType.BOT);
+
+    }
+}
