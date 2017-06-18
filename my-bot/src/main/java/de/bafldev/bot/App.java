aaf8225b53abe33e656917f99e8a065348cffbd8
Ryan <thelasmaster444@gmail.com>
2017-06-18 12:40:52 +0200

    maven setup

---
+++ my-bot/src/main/java/de/bafldev/bot/App.java
@@ -0,0 +1,13 @@
+package de.bafldev.bot;
+import java.util.LinkedList;;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
